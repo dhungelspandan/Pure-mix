2993e975d4e152ae70a377d5b3e89aa0d5dfcf7f
dhungelspandan <dhungelspandan22@gmail.com>
2017-02-08 11:32:31 +0545

    all files added

---
+++ footer.php
@@ -0,0 +1,33 @@
+<!-- Footer section
+================================================== -->
+<footer>
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-12 col-sm-12">
+				<p class="wow fadeInUp"  data-wow-delay="0.3s">Copyright © 2016 Your Company Name - Designed by Tooplate</p>
+				<ul class="social-icon wow fadeInUp"  data-wow-delay="0.6s">
+					<li><a href="#" class="fa fa-facebook"></a></li>
+					<li><a href="#" class="fa fa-twitter"></a></li>
+					<li><a href="#" class="fa fa-dribbble"></a></li>
+					<li><a href="#" class="fa fa-behance"></a></li>
+					<li><a href="#" class="fa fa-google-plus"></a></li>
+				</ul>
+			</div>
+			
+		</div>
+	</div>
+</footer>
+
+
+<!-- Javascript 
+================================================== -->
+<script src="<?php bloginfo('template_directory'); ?>/js/jquery.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/isotope.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/imagesloaded.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/wow.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/custom.js"></script>
+<?php wp_footer(); ?>
+</body>
+</html>
